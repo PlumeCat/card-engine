745408c2468b929e5002fbe7a8fdc9e419087c73
James <jamesmjdm@gmail.com>
2021-07-05 22:30:22 +0100

    added cors

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@
 /**/
 
 import express from "express"
+import cors from "cors"
 import { dealCards } from "./deck.js"
 import { TurnStateHandlers, checkNope } from "./turn.js"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.static("./dist/"))
 app.use(express.static("./server/"))
